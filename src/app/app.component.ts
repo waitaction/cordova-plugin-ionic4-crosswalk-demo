7b843bac060d2314bd41e49fe73507893cf86f30
chenqj <chenqijing@wo.cn>
2019-11-28 16:16:44 +0800

    自动选择webview内核

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare var Ionic: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -23,6 +25,10 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       setTimeout(() => {
+        // 在下次启动APP时使用CrosswalkWebView内核后
+        // Ionic.WebView.useCrosswalkWebViewAtTheNextStartup();
+        // 在下次启动APP时使用SystemkWebView内核
+        // Ionic.WebView.useSystemWebViewAtTheNextStartup();
         alert(window.navigator.userAgent);
       }, 2000);
 
